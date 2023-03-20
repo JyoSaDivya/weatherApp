4c54bfec15680b61f2277b9f12005f4fad974ce6
jyoti <jyoti123medge@gmail.com>
2023-03-20 08:34:01 +0530

    first commit

---
+++ src/Editor.js
@@ -0,0 +1,6 @@
+function Editor() {
+  return <h2>Hi, I am a Editor!</h2>;
+	
+}
+
+export default Editor;
---
+++ src/Iframe.js
@@ -0,0 +1,7 @@
+function Iframe() {
+  return <h2>Hi, I am second Editor!</h2>;
+  return <h2>I am third editor</h2>;
+	
+}
+
+export default Iframe;
